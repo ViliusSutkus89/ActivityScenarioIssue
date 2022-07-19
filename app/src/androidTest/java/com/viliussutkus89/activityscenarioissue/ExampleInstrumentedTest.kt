c98fff5a397ef3b0354135fa6aa6c2069333bde6
Vilius Sutkus '89 <ViliusSutkus89@gmail.com>
2022-07-19 18:17:41 +0300

    Use ActivityScenario

--- app/src/androidTest/java/com/viliussutkus89/activityscenarioissue/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/viliussutkus89/activityscenarioissue/ExampleInstrumentedTest.kt
@@ -1,8 +1,9 @@
 package com.viliussutkus89.activityscenarioissue
 
+import androidx.test.core.app.ActivityScenario
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
-
+import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 
@@ -15,6 +16,14 @@ import org.junit.Assert.*
  */
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
+
+    @Before
+    fun setUp() {
+        ActivityScenario.launch(MainActivity::class.java).onActivity { activity ->
+            assertNotNull(activity)
+        }
+    }
+
     @Test
     fun useAppContext() {
         // Context of the app under test.
